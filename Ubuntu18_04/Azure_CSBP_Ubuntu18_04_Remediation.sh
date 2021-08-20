3a664617a82693e9aadf7644b515a89750b9428c
sorcia25 <erickmex25@gmail.com>
2021-08-20 17:17:28 -0500

    Create Azure_CSBP_Ubuntu18_04_Remediation.sh

---
+++ Ubuntu18_04/Azure_CSBP_Ubuntu18_04_Remediation.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+: '
+#SYNOPSIS
+    Quick win script for remediation of Ubuntu baseline misconfigurations.
+.DESCRIPTION
+    This script aims to remediate all possible OS baseline misconfigurations for Ubuntu 18.04 based Virtual machines on Azure.
+
+.NOTES
+
+    Copyright (c) Toño Maldonado - None rights reserved :OP
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    Version: 1.0
+    # PREREQUISITE
+
+.EXAMPLE
+    Command to execute : bash Azure_CSBP_Ubuntu18_04_Remediation.sh
+.INPUTS
+
+.OUTPUTS
+    None
+'
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;35m'
+NC='\033[0m'
+
+success=0
+fail=0
+
+##Category Initial Setup - Additional Process Hardening
+echo
+echo -e "${BLUE} Initial Setup - Additional Process Hardening${NC}"
+
+# 1.5.3 Ensure address space layout randomization (ASLR) is enabled
+echo
+echo -e "${RED}1.5.3${NC} Ensure address space layout randomization (ASLR) is enabled"
+egrep -q "^(\s*)kernel.randomize_va_space\s*=\s*\S+(\s*#.*)?\s*$" /etc/sysctl.conf && sed -ri "s/^(\s*)kernel.randomize_va_space\s*=\s*\S+(\s*#.*)?\s*$/\1kernel.randomize_va_space = 2\2/" /etc/sysctl.conf || echo "kernel.randomize_va_space = 2" >> /etc/sysctl.conf
+policystatus=$?
+if [[ "$policystatus" -eq 0 ]]; then
+    echo -e "${GREEN}Remediated:${NC} Ensure address space layout randomization (ASLR) is enabled"
+    success=$((success + 1))
+else
+    echo -e "${RED}UnableToRemediate:${NC} Ensure address space layout randomization (ASLR) is enabled"
+    fail=$((fail + 1))
+fi
+
+##Category 2.1 Services - inetd Services
+echo
+echo -e "${BLUE}2.1 Services - inetd Services${NC}"
+
+# 2.2.10 Ensure xinetd is not enabled
+echo
+echo -e "${RED}2.1.10${NC} Ensure xinetd is not enabled"
+systemctl disable xinetd
+policystatus=$?
+if [[ "$policystatus" -eq 0 ]]; then
+    echo -e "${GREEN}Remediated:${NC} Ensure xinetd is not enabled"
+    success=$((success + 1))
+else
+    echo -e "${RED}UnableToRemediate:${NC} Ensure xinetd is not enabled"
+    fail=$((fail + 1))
+fi
