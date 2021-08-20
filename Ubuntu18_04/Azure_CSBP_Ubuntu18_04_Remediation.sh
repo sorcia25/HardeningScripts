4e9cfd7b3070d0189177cf9bb562659ee893c94c
sorcia25 <erickmex25@gmail.com>
2021-08-20 18:56:23 -0500

    Update Azure_CSBP_Ubuntu18_04_Remediation.sh

--- Ubuntu18_04/Azure_CSBP_Ubuntu18_04_Remediation.sh
+++ Ubuntu18_04/Azure_CSBP_Ubuntu18_04_Remediation.sh
@@ -120,13 +120,13 @@ fi
 echo
 echo -e "${RED}1.1.2${NC} Ensure separate partition exists for /tmp"
 echo -e "${YELLOW} This setting isn't applicable for an Azure VM"
-echo -e "${PURPLE} REASON:{NC} This setting must be configured during the installation, and in this case the VM was created based on a Azure image"
+echo -e "${PURPLE} REASON:${NC} This setting must be configured during the installation, and in this case the VM was created based on a Azure image"
 
 # 1.1.3  Ensure nodev option set on /tmp partition 
 echo
 echo -e "${RED}1.1.3${NC}  Ensure nodev option set on /tmp partition "
 echo -e "${YELLOW} This setting isn't applicable for an Azure VM"
-echo -e "${PURPLE} REASON:{NC} The /tmp partition isn't exist"
+echo -e "${PURPLE} REASON:${NC} The /tmp partition isn't exist"
 
 # 1.1.20 Ensure sticky bit is set on all world-writable directories
 echo
